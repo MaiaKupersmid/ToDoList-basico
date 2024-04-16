cb2ce93c58c7a05d29bcca399ba2990ad45b1910
JULIETA IZBICKI <47350196@ort>
2024-04-16 07:48:22 -0300

    listo

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 var lista = [
-    {
-        id: 0,
-        item: "prueba lengua",
-        fecha: new Date().getTime(),
-        check: false,
-        tacho: '',
-        fin: "",
-    }
 ]
 
 function AgregarItem() {
@@ -25,16 +17,9 @@ function recargarItems() {
     var h = document.getElementById("checklist");
     h.innerHTML= ``;
     lista.forEach(function(item) {
-    h.innerHTML += `
-        <input type="checkbox" onclick="Tachado(${item.id})" ${item.check ? 'checked' : ''} id="${item.id}">
-        <label>${item.item} - ${formatDate(new Date(item.fecha))}</label><br>`;
-        if (item.check==true)
-        {
-            h.innerHTML +=`
-                <label>- ${finDate(new Date(item.fin))}</label>
-                `;
-            
-        }
+        h.innerHTML += `
+            <input type="checkbox" onclick="Tachado(${item.id})" ${item.check ? 'checked' : ''} id="${item.id}">
+            <label>${item.item} - ${formatDate(new Date(item.fecha))}</label><br>`;
     });
 }
 
@@ -48,26 +33,26 @@ function finDate(date)
 }
 
 function Tachado(id) {
-    const txt = document.getElementById(id);
-    const tarea = lista.find(t => t.id === id);
-    tarea.tacho = new Date().getTime();
-    tarea.check = true;
+    for(let i = 0; i< lista.length; i++){
+        if (lista[i].id == id){
+            lista[i].tacho = new Date().getTime();
+            lista[i].check = true;
+        }
+    }
 }
 
 function rapido(){
     var h = document.getElementById("checklist");
     h.innerHTML= ``;
     let masRa = 0;
-    let cual = ""
+    let cual = '';
     console.log(lista);
     for(let i = 0; i < lista.length; i++){
         if(masRa<lista[i].tacho){
+            console.log("ahora: " + lista[i].item)
             masRa=lista[i].tacho;
-            cual = lista[i].item
+            cual = lista[i].item;
         }
     }
     h.innerHTML= `<p> El mas rapido en realizarse fue ${cual}</p>`;
-    lista.forEach(function(item) {
-
-    });
 }
